ff961aa48ed481303cfb14d9fa44904126dd954a
jellyzone <jellyzone@gmail.com>
2017-11-10 18:43:21 +0800

    ""

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -27,7 +27,7 @@ public:
     void initialise (const String& commandLine) override
     {
         // This method is where you should put your application's initialisation code..
-
+		(commandLine);
         mainWindow = new MainWindow (getApplicationName());
     }
 
@@ -51,6 +51,7 @@ public:
         // When another instance of the app is launched while this one is running,
         // this method is invoked, and the commandLine parameter tells you what
         // the other instance's command-line arguments were.
+		(commandLine);
     }
 
     //==============================================================================
