969d6d6a8a253c9f29b8a74a9021912e5c6caeb0
jellyzone <jellyzone@gmail.com>
2017-11-15 19:52:21 +0800

    tutorial

--- Source/glsl.cpp
+++ Source/glsl.cpp
@@ -17,31 +17,47 @@ extern vec2 iResolution ;
 
 
 
-#define S(a, b, t) smoothstep(a,b,t);
+#define S(a, b, t) smoothstep(a,b,t)
+#define sat(x) clamp(x, 0.f, 1.f)
 
-vec4 Eye(vec2 uv)
+
+
+float remap01(float a, float b, float t)
 {
-	vec4 col = vec4(0.);
-
-	return col;
+	float s1 = (t - a) / (b - a);
+	float s2 = sat(s1);
+	return s2;
+	
+	//return sat((t - a) / (b - a));
 }
 
-vec4 Mouth(vec2 uv)
+float remap(float a, float b, float c, float d, float t)
 {
-	vec4 col = vec4(0.);
-
-	return col;
+	return (t - a) / (b - a) * (d - c) + c;
 }
+
+//vec4 Eye(vec2 uv)
+//{
+//	vec4 col = vec4(0.);
+//
+//	return col;
+//}
+//
+//vec4 Mouth(vec2 uv)
+//{
+//	vec4 col = vec4(0.);
+//
+//	return col;
+//}
 
 vec4 Head(vec2 uv)
 {
-	vec4 col = vec4(0.9, .65, .1, 1);
+	vec4 col = vec4(0.9f, .65f, .1f, 1);
 
 	float d = length(uv);
 
 
 	col.a = S(.5f, .49f, d);
-
 	/*
 	if(d >.5)
 	col.a = 0;
@@ -51,26 +67,39 @@ vec4 Head(vec2 uv)
 	col.a = 0.5;   // smooth step
 	*/
 
+	float edgeShade = remap01(0.35f, .5f, d);
+
+	edgeShade *= edgeShade;
+
+	col.rgb = col.rgb * (1.f - edgeShade*.5f);
+	
+
+	col.rgb = mix((vec3)col.rgb, vec3(.6f, .3f, .1f), S(.47f, .48f, d));
+
+	//float ss =S(.47f, .48f, d);
+	//col.r = mix(col.r, .6f, ss);
+	//col.g = mix(col.g, .3f, ss);
+	//col.b = mix(col.b, .1f, ss);
 	return col;
 }
 
 
 vec4 smiley(vec2 uv)
 {
-	vec4 col = vec4(0.);
+	vec4 col = vec4(.0f, .0f, .0f, 1.f);
 	vec4 head = Head(uv);
+
+	//head.a = 1.;
+	//return head; 
+
 	col = mix(col, head, head.a);
 	return col;
 }
 void mainImage(vec2 gl_FragCoord)
 {
 	vec2 uv = gl_FragCoord.xy() / iResolution.xy();
-	uv -= 0.5f;
+	uv -= 0.5;
 	uv.x *= iResolution.x / iResolution.y;
+	fragColor = smiley(uv);
 
-
-
-
-
-	fragColor = smiley(uv);;
 }
